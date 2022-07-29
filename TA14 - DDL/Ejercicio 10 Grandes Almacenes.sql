04e0b6724343ef6453d37badcd15dd991630eccc
IzanDuesoLopez <108723551+IzanDuesoLopez@users.noreply.github.com>
2022-07-29 20:56:56 +0200

    Corrección

--- TA14 - DDL/Ejercicio 10 Grandes Almacenes.sql
+++ TA14 - DDL/Ejercicio 10 Grandes Almacenes.sql
@@ -11,19 +11,18 @@ create table Cajeros (
 	codigo char(5) primary key,
     nomApels nvarchar(100)
 	);
-    
-    create table Venta (
-	foreign key (codigo) references Cajeros (codigo),
-    foreign key (codigo) references Maquinas_Registradoras (codigo),
-    foreign key (codigo) references Productos (codigo)
-	);
-    
-    create table Maquinas_Registradoras (
+create table Maquinas_Registradoras (
 	codigo int auto_increment primary key,
     piso int
 	);
-
-show columns from Productos;
-show columns from Cajeros;
-show columns from Maquinas_Registradoras;
-show columns from Venta;
+	
+    create table Venta (
+	codigo_cajero char(5),
+	codigo_maquina int,
+	codigo_producto int,
+	primary key (codigo_cajero, codigo_maquina, codigo_producto),
+	foreign key (codigo_cajero) references Cajeros (codigo),
+    foreign key (codigo_maquina) references Maquinas_Registradoras (codigo),
+    foreign key (codigo_producto) references Productos (codigo)
+	);
+    
